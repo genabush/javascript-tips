d31469968edaf0419a5c7394f18843ed2c4201fa
Gennadiy Kulabukhov <genabush1990@gmail.com>
2021-08-07 08:48:33 +0300

    added numbers methods

--- js/numbers.js
+++ js/numbers.js
@@ -1,2 +1,13 @@
 // Number and all number methods
-const num = 100;
+const num = 100.00;
+
+// The toFixed() method formats a number using fixed-point notation.
+num.toFixed(0); // 100
+
+// The Number.parseInt() method parses a string argument and returns an integer of the specified radix or base.
+Number.parseInt(num) // 100
+
+// The toString() method returns a string representing the specified Number object.
+num.toString(); // '100'
+
+
